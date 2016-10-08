27edd82bcc764e1c82b0f35a5566a1ff6f761d84
Dmitrijs Valaks <dmitry.valak@gmail.com>
2016-10-08 11:28:27 +0300

    fixes for module1 task2

--- src/js/check.js
+++ src/js/check.js
@@ -1,13 +1,11 @@
 "use strict";
 
-
 function getMessage(a, b) {
 
   var message = "Переданы некорректные данные";
-
+  var i;
 
   if(typeof a == "boolean") {
-
       if(a == true) {
         message = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
       } else {
@@ -15,32 +13,23 @@ function getMessage(a, b) {
       }
   }
 
-
   if(typeof a == "number") {
-    message = "Переданное SVG-изображение содержит " + a + " объектов и "+ b*4 +" атрибутов";
+    message = "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
   }
 
-
   if(Array.isArray(a)) {
-
     var amountOfRedPoints = 0;
-
-    for(var i = 0; i < a.length; i++) {
+    for(i = 0; i < a.length; i++) {
       amountOfRedPoints = amountOfRedPoints + a[i];
     }
-
     message = "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
   }
 
-
   if(Array.isArray(a) && Array.isArray(b)) {
-
     var artifactsSquare = 0;
-
-    for(var i = 0; i < a.length; i++) {
+    for(i = 0; i < a.length; i++) {
       artifactsSquare = artifactsSquare + (a[i] * b[i]);
     }
-
     message = "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
   }
 
